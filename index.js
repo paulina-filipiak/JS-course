7efcffdf0179664080911f9a061515ae9d3fae33
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-21 10:23:15 +0200

    created first function

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-
 function increment() {
-    count += 1
-    countEl.textContent = count
+  console.log("The button was clicked");
 }
-
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
-    count = 0
-}
-
-console.log("Let's count people on the subway!")
