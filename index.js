b6275e0cdd94affe69943d24ae608e8aad7aabf1
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-21 13:11:32 +0200

    added reset count to save button

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-  count = count + 1;
+  count += 1;
   console.log(count);
   countEl.innerText = count;
 }
 
+let paragraphText = document.getElementById("save-el");
+
 function save() {
-  console.log(count);
+  console.log("paragraphText");
+  let savedEntries = count + " - ";
+  paragraphText.textContent += savedEntries;
+  count = 0;
+  countEl.innerText = count;
 }
