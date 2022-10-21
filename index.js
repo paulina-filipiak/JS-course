219f6d9e02fa44f49079e0fa24cce3c8eed599d8
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-21 11:41:57 +0200

    created Save button

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let countEl = document.getElementById("count-el");
 
-console.log(countEl);
-
 let count = 0;
 
 function increment() {
   count = count + 1;
   console.log(count);
   countEl.innerText = count;
+}
+
+function save() {
+  console.log(count);
 }
