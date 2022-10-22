8771606ec1b62b5fc79757c7be3008a9bd95e2f8
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-22 13:02:22 +0200

    added some DOM elements to index.js

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ let hasBlackjack = false;
 let isAlive = true;
 
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-  console.log(message);
-} else if (sum === 21) {
-  message = "You've got blackjack!";
-  console.log(message);
-  hasBlackjack = true;
-} else {
-  message = "You are out of the game";
-  console.log(message);
-  isAlive = false;
+function startGame() {
+  sumEl.textContent += " " + sum;
+  cardsEl.textContent += " " + firstCard + " " + secondCard;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "You are out of the game";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
-
-console.log(hasBlackjack);
