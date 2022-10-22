22e552fa2cc427253b3c028eaa2d7b8a6df33e6a
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-22 12:13:39 +0200

    added first if else statement

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let message = "";
+
+if (sum <= 20) {
+  message = "Do you want to draw a new card?";
+  console.log(message);
+} else if (sum === 21) {
+  message = "You've got blackjack!";
+  console.log(message);
+  hasBlackjack = true;
+} else {
+  message = "You are out of the game";
+  console.log(message);
+  isAlive = false;
+}
+
+console.log(hasBlackjack);
