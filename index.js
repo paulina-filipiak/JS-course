ea1023716d53628779ee07a632ebeff7c3094f28
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-10-31 12:39:20 +0100

    created myLeads array and added content from input to it

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el")
 
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value;);
+  console.log(myLeads);
+});
