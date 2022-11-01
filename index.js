4c2632e20e70d4cf5a36517e93b6ea02395a740b
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-11-01 13:16:02 +0100

    added myLeads array to local storage

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let listItems = ""
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
+  console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
+  let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
     <li>
